3a501855e4d4b2b79475de9a81a8c5ca64c10251
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-05-16 13:55:25 +0530

    bug fixed

--- src/Components/NavGraph.jsx
+++ src/Components/NavGraph.jsx
@@ -9,8 +9,8 @@ const NavGraph = () => {
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     xmlnsXlink="http://www.w3.org/1999/xlink"
-                    width="1250"
-                    height="2020"
+                    width="100%"
+                    height="100%"
                     fill="none"
                     viewBox="0 0 1595 2022"
                 >
